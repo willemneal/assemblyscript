0df4378c0b9db3115251e1c97799935db0f5068b
Willem Wyndham <willem.neal@gmail.com>
2019-06-12 22:36:20 -0400

    Added test package for installing packages

---
+++ tests/packages/npm-reader/assembly/__tests__/package.json.spec.ts
@@ -0,0 +1,36 @@
+import { JSON } from "assemblyscript-json";
+
+const package_str = `\
+{\
+    "name": "npm-reader",\
+    "version": "0.0.2",\
+    "description": "Reads package.json files",\
+    "scripts": {\
+      "test": "asp",\
+      "build": "asc assembly/index.ts --path node_modules --noEmit --runtime stub"\
+    },\
+    "keywords": [\
+      "AssemblyScript"\
+    ],\
+    "author": "Willem Wyndham",\
+    "license": "MIT",\
+    "dependencies": {\
+      "as-pect": "github:jtenner/as-pect",\
+      "assemblyscript": "file:../../..",\
+      "assemblyscript-json": "file:../../../../../../c/assemblyscript-json"\
+    }\
+}  \
+`
+
+let _package: JSON.Obj;
+describe("package.json", () => {
+    beforeAll(() => {
+        _package =  JSON.parse(package_str);
+    })
+    it("can have a scripts object", () => {
+        let scripts: JSON.Obj = _package.get("scripts");
+        expect<string>(scripts.get("test").str).toStrictEqual("asp");
+    })
+
+
+})
---
+++ tests/packages/npm-reader/assembly/index.ts
@@ -0,0 +1,4 @@
+
+import { JSON } from "assemblyscript-json";
+
+let obj = JSON.parse('{"hello":"world"}');
